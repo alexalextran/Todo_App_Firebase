ad019d99ec8d5ae5ee351e58655d117b57e623f6
alexalextran <alextran8385@gmail.com>
2022-02-24 01:02:10 +1100

    this is the last proper todo app im gonna do

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import '../src/sass/App.css';
-import Loginpage from "./pages/LoginPage";
+import './App.css';
+import Sidebar from './components/Sidebar'
+import Main from './components/Main'
+import User from './components/User'
+import AddNewTodo from './components/AddNewTodo'
+import Calendar from './components/Calendar'
+import Projects from './components/Projects'
+import Todos from './components/Todos'
+import EditTodo from './components/EditTodo'
+
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
     <div className="App">
-      
-      <Routes>
-        
-        <Route exact path="/" element={<Loginpage />} />
-    
-      </Routes>
-   
-    
+      <Sidebar>
+        <User />
+        <AddNewTodo />
+        <Calendar />
+        <Projects />
+      </Sidebar>
+      <Main>
+        <Todos />
+        <EditTodo />
+      </Main>
     </div>
-  </Router>
   );
 }
 
