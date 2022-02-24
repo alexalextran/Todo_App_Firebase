960529e9810e962af5c1bc29da30f1d74f0710e9
alexalextran <alextran8385@gmail.com>
2022-02-24 18:49:12 +1100

    delete function

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,17 @@
 import React, {useState} from 'react'
 import { ArrowClockwise, CheckCircleFill, Circle, Trash } from 'react-bootstrap-icons'
+import firebase from '../firebase'
 
 function Todo({todo}){
     const [hover, setHover] = useState(false)
+
+    const deleteTodo = todo => {
+        firebase
+            .firestore()
+            .collection('todos')
+            .doc(todo.id)
+            .delete()
+    }
 
     return (
         <div className='Todo'>
@@ -36,7 +45,10 @@ function Todo({todo}){
                         </span>
                     }
                 </div>
-                <div className="delete-todo">
+                <div
+                    className="delete-todo"
+                    onClick={ () => deleteTodo(todo)}
+                >
                     {
                         (hover || todo.checked) &&
                         <span>
